360aadf9812177584683dcb8401c8e24ae8323f4
David Ko <david.dk.ko@gmail.com>
2015-03-31 10:28:06 -0700

    Fixed type mismatch in robot protocol

--- library/src/linkbot.cpp
+++ library/src/linkbot.cpp
@@ -279,7 +279,7 @@ void Linkbot::getJointSafetyThresholds(int& t1, int& t2, int& t3)
 void Linkbot::getJointSafetyAngles(double& t1, double& t2, double& t3)
 {
     try {
-        auto rad2deg = [] (double x) {return x*180.0/M_PI;};
+        auto rad2deg = [] (double x) -> double {return x*180.0/M_PI;};
         auto value = asyncFire(m->robot,
             MethodIn::getMotorControllerSafetyAngle{},
             requestTimeout(), use_future).get();
