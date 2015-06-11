4744f95266ffdc7a8e1407749bf55c638cc31539
Harris Hancock <vortrab@gmail.com>
2015-06-11 12:06:29 -0700

    Add comment to log API

--- common/include/baromesh/log.hpp
+++ common/include/baromesh/log.hpp
@@ -11,7 +11,16 @@
 namespace baromesh {
 namespace log {
 
+// Build and return a description of what command line options the logging
+// system supports. Use the returned value to parse the command line, then pass
+// the generated variables_map to initialize.
+// If logFileName is specified, the description will configure the logging
+// system to log to that file by default, unless overridden on the command
+// line by --log-file.
 boost::program_options::options_description optionsDescription (boost::optional<std::string> logFileName);
+
+// Add some common attributes to the logging core and enable sinks as specified
+// by the configuring variables_map.
 void initialize (std::string appName, const boost::program_options::variables_map& conf);
 
 } // namespace log
