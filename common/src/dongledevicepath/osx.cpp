ac19c54710ebc6fd55b149e3c3aa58816410f110
Harris Hancock <vortrab@gmail.com>
2015-10-27 15:23:23 -0700

    Fix OS X <= 10.10

--- common/src/dongledevicepath/osx.cpp
+++ common/src/dongledevicepath/osx.cpp
@@ -3,12 +3,17 @@
 #include <boost/log/sources/logger.hpp>
 #include <boost/log/sources/record_ostream.hpp>
 
+#include <boost/lexical_cast.hpp>
+#include <boost/algorithm/string/split.hpp>
+
 #include "CF++.h"
 
 #include <IOKit/IOKitLib.h>
 #include <IOKit/IOBSD.h>
 #include <IOKit/usb/IOUSBLib.h>
 #include <IOKit/IOReturn.h>
+
+#include <sys/sysctl.h>
 
 #include <algorithm>
 
@@ -28,14 +33,31 @@ static CF::String getStringProperty (io_object_t device, CF::String key, bool re
     return value;
 }
 
-static UniqueIoObject getUsbDeviceIterator () {
-    // OS X 10.11 overhauled the USB system, introducing the IOUSBHostDevice
-    // class name.
-    auto classes = IOServiceMatching("IOUSBHostDevice");
-    if (!classes) {
-        // Fall back to the IOUSBDevice class name--we are on 10.10 or less.
-        classes = IOServiceMatching(kIOUSBDeviceClassName);
+static unsigned darwinVersionMajor () {
+    char osRelease[256];
+    size_t osReleaseSize = sizeof(osRelease);
+    auto rc = sysctlbyname("kern.osrelease", osRelease, &osReleaseSize, nullptr, 0);
+    if (rc) {
+        throw std::runtime_error("Error getting Darwin version string with sysctlbyname");
     }
+    assert(osReleaseSize <= sizeof(osRelease));
+    auto darwinVersionString = std::string(osRelease, osReleaseSize);
+
+    std::vector<decltype(darwinVersionString)> splitResult;
+    using CharT = decltype(darwinVersionString)::value_type;
+    boost::split(splitResult, darwinVersionString, [] (CharT c) { return c == '.'; });
+    if (splitResult.size() < 3) {
+        throw std::runtime_error("Error parsing Darwin version string");
+    }
+    return boost::lexical_cast<unsigned>(splitResult[0]);
+}
+
+static UniqueIoObject getUsbDeviceIterator () {
+    // OS X 10.11 (Darwin 15) overhauled the USB system, introducing the
+    // IOUSBHostDevice class name.
+    auto classes = IOServiceMatching(darwinVersionMajor() < 15
+                                     ? kIOUSBDeviceClassName
+                                     : "IOUSBHostDevice");
     io_iterator_t iter;
     auto kr = IOServiceGetMatchingServices(kIOMasterPortDefault, classes, &iter);
     if (kIOReturnSuccess != kr) {
@@ -46,6 +68,7 @@ static UniqueIoObject getUsbDeviceIterator () {
 
 int dongleDevicePathImpl (char *buf, size_t len) {
     boost::log::sources::logger lg;
+
     auto iter = getUsbDeviceIterator();
     while (auto device = UniqueIoObject{IOIteratorNext(iter)}) {
         for (auto i = 0; i < NUM_BAROBO_USB_DONGLE_IDS; ++i) {
@@ -76,6 +99,10 @@ int dongleDevicePathImpl (char *buf, size_t len) {
                     expectedProduct.end(),
                     productValue.begin()).first) {
                 auto path = std::string(getStringProperty(device, "IOCalloutDevice", true));
+                if (!path.length()) {
+                    BOOST_LOG(lg) << "Found dongle in IORegistry, but no IOCalloutDevice";
+                    continue;
+                }
                 if (!access(path.c_str(), R_OK | W_OK)) {
                     // Success!
                     strncpy(buf, path.c_str(), len);
